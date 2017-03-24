1947b1f81d4d15621a293dff70f5ff8d01a51967
kisejun <kisejun@218.38.137.28>
2017-03-24 14:41:04 +0900

    car2

--- JavaPractice/src/org/dimigo/oop/CarTest2.java
+++ JavaPractice/src/org/dimigo/oop/CarTest2.java
@@ -23,8 +23,8 @@ public class CarTest2 {
 		// TODO Auto-generated method stub
 
 		
-		Car car = new Car();
-		System.out.println("<< 자동차 목록 >>");
+		Car car = new Car("현대자동차","제네시스","검정색",225,50000000);
+		
 		System.out.println("제조사명 : " + car.getCompany());
 		System.out.println("모델명 : "+car.getModel());
 		System.out.println("색상 : "+ car.getColor());
@@ -34,12 +34,7 @@ public class CarTest2 {
 		
 		
 		
-		Car car2 = new Car();
-		car2.setCompany("기아자동차");
-		car2.setModel("K7");
-		car2.setColor("흰색");
-		car2.setMaxSpeed(246);
-		car2.setPrice(40000000);
+		Car car2 = new Car("기아자동차","K7","흰색",246,40000000);
 		
 		System.out.println("제조사명 : "+ car2.getCompany());
 		System.out.println("모델명 : "+car2.getModel());
@@ -48,12 +43,7 @@ public class CarTest2 {
 		System.out.println("가격 : "+ String.format("%,d", car2.getPrice()) + "원");
 		System.out.println("");
 		
-		Car car3 = new Car();
-		car3.setCompany("삼성자동차");
-		car3.setModel("SM7");
-		car3.setColor("회색");
-		car3.setMaxSpeed(200);
-		car3.setPrice(38000000);
+		Car car3 = new Car("삼성자동차","SM7","회색",200,38000000);
 		
 		System.out.println("제조사명 : "+car3.getCompany());
 		System.out.println("모델명 : "+car3.getModel());
