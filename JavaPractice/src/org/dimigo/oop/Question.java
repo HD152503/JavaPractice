1bd4e561df3231d057bb3045d3bc8a02f9808ce5
kisejun <kisejun@218.38.137.28>
2017-04-04 12:03:27 +0900

    Question

---
+++ JavaPractice/src/org/dimigo/oop/Question.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package org.dimigo.oop;
+
+import java.util.Scanner;
+
+/**
+* <pre>
+*  org.dimigo.oop
+*	|_ Question
+*
+* 1. 개요 :
+* 2. 작성일 : 2017. 3. 31.
+* </pre>
+ * @author : kisejun
+ * @version : 1.0
+ */
+public class Question {
+	public static void main(String[] args) {
+		String[] questions = {"1. 가장 좋아하는 가수는?", "2. 가장 좋아하는 배우는?", "3. 가장 좋아하는 과목은?"};
+		String[] answers = {"트와이스","원빈", "응용 프로그래밍"};
+		
+		Scanner scanner = new Scanner(System.in);
+		StringBuilder sb = new StringBuilder();
+		for(int i=0; i<3; i++){
+			System.out.println(questions[i]);
+			String answer = scanner.nextLine();
+			System.out.println((answer.equals(answers[i]))?"정답입니다!":"틀렸습니다!");
+		
+			sb.append(questions[i]+" ").append(answers[i]).append(" 입니다.").append("\n");
+
+		}
+		
+		System.out.println("<<결과 출력>>");
+		System.out.println(sb.toString());
+		
+	}
+
+}
