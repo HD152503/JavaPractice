a83fed8005edb85492e7678ab9a7b9628c116fe8
kisejun <kisejun@218.38.137.28>
2017-04-06 09:51:23 +0900

    Snack

---
+++ JavaPractice/src/org/dimigo/oop/SnackTest.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package org.dimigo.oop;
+
+/**
+* <pre>
+*  org.dimigo.oop
+*	|_ SnackTest
+*
+* 1. 개요 :
+* 2. 작성일 : 2017. 4. 6.
+* </pre>
+ * @author : kisejun
+ * @version : 1.0
+ */
+public class SnackTest {
+
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		String[] Snack = new String[3];
+		
+	}
+}
