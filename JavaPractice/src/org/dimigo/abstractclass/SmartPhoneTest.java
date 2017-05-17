fa4783cb9ad20a4512fc17843bdb9458e67c2f9d
kisejun <kisejun@218.38.137.28>
2017-05-17 15:24:27 +0900

    abstract

---
+++ JavaPractice/src/org/dimigo/abstractclass/SmartPhoneTest.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package org.dimigo.abstractclass;
+
+/**
+* <pre>
+*  org.dimigo.inheritance
+*	|_ SmartPhoneTest
+*
+* 1. 개요 :
+* 2. 작성일 : 2017. 5. 12.
+* </pre>
+ * @author : kisejun
+ * @version : 1.0
+ */
+public class SmartPhoneTest {
+	public static void main(String[] args) {
+	
+		SmartPhone[] sp ={ 
+			new IPhone("iPhone 7","애플", 900000),
+			new Galaxy("갤럭시 S8","삼성", 800000)
+			
+		};
+		for(int i=0; i<2; i++){
+			System.out.println(sp[i]);
+			sp[i].turnOn();
+			sp[i].pay();
+			sp[i].useSpecialFunction();
+			sp[i].turnOff();
+		
+		}
+	}
+
+}
