61fc1bc698e17d6415c3b0fc08bfeb2409cda09a
kisejun <kisejun@218.38.137.28>
2017-05-24 16:26:38 +0900

    sad

---
+++ JavaPractice/src/org/dimigo/interfaces/IDBManager.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package org.dimigo.interfaces;
+
+/**
+* <pre>
+*  org.dimigo.interfaces
+*	|_ IDBManager
+*
+* 1. 개요 :
+* 2. 작성일 : 2017. 5. 24.
+* </pre>
+ * @author : kisejun
+ * @version : 1.0
+ */
+public interface IDBManager {
+public static final String ORACLE_DATABASE = "ORACLE";
+public static final String SYBASE_DATABASE = "SYBASE";
+public void insert();
+public void search();
+public void update();
+public void delete();
+public static IDBManager getDBObject(String db){
+	if(db.equals(ORACLE_DATABASE)){
+		return new OracleDB();
+	} else if (db.equals(SYBASE_DATABASE)){
+		return new SybaseDB();
+	}
+	return null;
+}
+}
