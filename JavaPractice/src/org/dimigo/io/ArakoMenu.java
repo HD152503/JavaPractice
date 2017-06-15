226d607f2c2f366fe7ae48c335abaf0343ed1b7b
kisejun <kisejun@192.168.0.37>
2017-06-15 22:28:40 +0900

    arakomenu

--- JavaPractice/src/org/dimigo/io/ArakoMenu.java
+++ JavaPractice/src/org/dimigo/io/ArakoMenu.java
@@ -25,24 +25,27 @@ public class ArakoMenu {
 
 	public static void main(String[] args) {
 		try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-			 Writer writer = new BufferedWriter(new FileWriter("files/menu.txt"));
-			 BufferedReader reader = new BufferedReader(new FileReader("files/menu.txt"))) {
+				Writer writer = new BufferedWriter(new FileWriter("files/menu.txt"));
+				BufferedReader reader = new BufferedReader(new FileReader("files/menu.txt"));) {
 			
 			String str;
 			
 			while ((str = br.readLine()) != null) {
-				writer.write(str + "\n");
+				writer.write(str+"\n");
 			}
-			writer.flush();
 			
 			System.out.println("<< 메뉴 출력 >>");
+			
+			writer.flush();
 			
 			while ((str = reader.readLine()) != null) {
 				System.out.println(str);
 			}
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		
 	}
 
 }
